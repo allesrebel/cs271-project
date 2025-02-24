a86dee1b00eafaf555961cb3ef8a8dd553ca21f9
Rebel <allesrebel@yahoo.com>
2025-02-24 00:36:42 -0800

    And we're off to the races!

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello world!')
+import tsplib95
+
+files = ["att48.tsp", "dantzig42.tsp", "fri26.tsp", "gr17.tsp", "p01.tsp"]
+tsp_data = {}
+
+for file in files:
+    tsp_data[file] = tsplib95.load('dataset/'+file)
+
+print(tsp_data["att48.tsp"])
